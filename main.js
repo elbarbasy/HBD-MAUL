b24756e9f94988dad1590454a155746398327385
Muhammad Amrizal <149764969+elbarbasy@users.noreply.github.com>
2025-05-10 01:07:39 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-function loadFlower() {
-  document.getElementById('flowerFrame').src = 'flower.html';
-  document.getElementById('flowerFrame').style.display = 'block';
-
-  // Sembunyikan elemen sebelumnya
-  document.querySelector('.greetings').style.display = 'none';
-  document.querySelector('.description').style.display = 'none';
-  document.querySelector('.button').style.display = 'none';
-}
-
-
 onload = () =>{
         document.body.classList.remove("container");
 };
